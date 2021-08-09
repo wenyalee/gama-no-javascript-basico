0000f420a27cf64bc4bfea41531771a690e6104d
wenyalee <88101187+wenyalee@users.noreply.github.com>
2021-08-09 02:23:00 -0300

    Mudanças para testar display de success na validacao de cpf

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log('Javascript carregado');
 
+function validaCPF(cpf) {
+    return true;
+}
+
 function validacao() {
     console.log('Iniciando validacao CPF');
 
     var cpf = document.getElementById('cpf_digitado').value;
-    console.log(cpf);
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    }
 }
