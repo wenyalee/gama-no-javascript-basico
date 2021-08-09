11c8779fd98beca70225f950e5fccfce9bd921e8
wenyalee <88101187+wenyalee@users.noreply.github.com>
2021-08-09 03:41:49 -0300

    mudanças para verificar cpf numero a numero

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 console.log('Javascript carregado');
 
 function validaCPF(cpf) {
-    return false;
+    console.log(cpf.length);
+    if (cpf.length != 11) {
+        return false;
+    } else {
+
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+        
+        var soma = 0;
+
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }    
+        console.log(soma);    
+            
+            // console.log('numeros do cpf ' + numeros);
+            // console.log('digito do cpf ' + digitos);
+
+        return true;
+    }
 }
 
 function validacao() {
